a99219de3a473911b90dc384d6b798535d3e0570
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-25 02:50:31 +0600

    dashboard menu items conditionally updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Routes from './Routes/Routes';
 import { Toaster } from 'react-hot-toast';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <div className="App">
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
-      <Routes></Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+        />
+        <Routes></Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,95 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Dashboard = () => {
+    const { user } = useContext(AuthContext)
+    const { data: filterdUser } = useQuery({
+        queryKey: ['users'],
+
+
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/user?email=${user?.email}`)
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
-        <div>
-            <h1>This is dashboard</h1>
+        <div className='flex flex-col md:flex-row'>
+            <div className=''>
+                <div className="flex md:h-screen flex-col justify-between border-r bg-white">
+                    <div className="px-4 py-6">
+                        <span className="flex items-center justify-center h-10 w-full rounded-lg bg-[#256D85] text-white font-bold">Dashboard</span>
+
+                        <nav aria-label="Main Nav" className="mt-6 flex flex-col space-y-1">
+                            {
+                                (filterdUser?.userType === 'Buyer') &&
+                                <Link
+                                    to="#"
+                                    className="flex items-center rounded-lg bg-gray-100 px-4 py-2 text-gray-700"
+                                >
+                                    <span className="ml-3 text-sm font-medium"> My Orders </span>
+                                </Link>
+                            }
+                            {
+                                (filterdUser?.userType === 'Seller') &&
+                                <>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+
+
+                                        <span className="ml-3 text-sm font-medium">Add A product</span>
+                                    </Link>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+                                        <span className="ml-3 text-sm font-medium"> My Products </span>
+                                    </Link>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+                                        <span className="ml-3 text-sm font-medium">My Buyers</span>
+                                    </Link>
+                                </>
+                            }
+                            {
+                                (filterdUser?.userType === 'Admin') &&
+                                <>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+                                        <span className="ml-3 text-sm font-medium">All Buyers</span>
+                                    </Link>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+                                        <span className="ml-3 text-sm font-medium">All Sellers</span>
+                                    </Link>
+                                    <Link
+                                        to="#"
+                                        className="flex items-center rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
+                                    >
+                                        <span className="ml-3 text-sm font-medium">Reported Items</span>
+                                    </Link>
+
+                                </>
+                            }
+                        </nav>
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <h1 className='text-5xl'> This is sample text</h1>
+            </div>
         </div>
     );
 };
