3ef2da21e2b465abdab1a7f862c333f0d7686ead
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-11-24 17:58:40 +0600

    login page design complete

--- src/pages/Home/Categories/SingleCategory/SingleCategory.js
+++ src/pages/Home/Categories/SingleCategory/SingleCategory.js
@@ -12,7 +12,7 @@ const SingleCategory = ({ category }) => {
             />
 
             <div className="relative p-8">
-                <p className="text-4xl font-bold text-white flex items-center absolute bottom-4">{name}</p>
+                <p className="text-4xl font-bold text-white flex items-center absolute bottom-2">{name}</p>
                 <div className="mt-64">
                     <div
                         className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,96 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleLoginSubmit = data => console.log(data);
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+                <h1 className="text-4xl font-semibold text-center text-[#256D85]  uppercase">
+                    Sign in
+                </h1>
+                <form onSubmit={handleSubmit(handleLoginSubmit)} className="mt-6">
+                    <div className="mb-2">
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            {...register("email", { required: true })}
+                            className="block w-full px-4 py-2 mt-2 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            {...register("password", { required: true })}
+                            className="block w-full px-4 py-2 mt-2bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <Link
+                        to="#"
+                        className="text-xs text-[#256D85]  hover:underline"
+                    >
+                        Forget Password?
+                    </Link>
+                    <div className="mt-6">
+                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#256D85] rounded-md hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px]">
+                            Login
+                        </button>
+                    </div>
+                </form>
+                <div className="relative flex items-center justify-center w-full mt-6 border border-t">
+                    <div className="absolute px-5 bg-white">Or</div>
+                </div>
+                <div className="flex mt-4 gap-x-2">
+                    <button
+                        type="button"
+                        className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 32 32"
+                            className="w-5 h-5 fill-current"
+                        >
+                            <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                        </svg> <span className='font-bold ml-2 text-[#256D85]'>Sing in with google </span>
+                    </button>
+
+
+                </div>
+
+                <p className="mt-8 text-xs font-light text-center text-gray-700">
+                    {" "}
+                    Don't have an account?{" "}
+                    <Link
+                        to="#"
+                        className="font-medium text-purple-600 hover:underline"
+                    >
+                        Sign up
+                    </Link>
+                </p>
+            </div>
         </div>
     );
-};
+}
+
 
 export default Login;
+
+{/* <form onSubmit={handleSubmit(onSubmit)}>
+<input type="email" placeholder="Email" {...register("Email", { required: true })} />
+<input type="password" placeholder="Password" {...register("Password", { required: true, min: 6, maxLength: 30 })} />
+
+<input type="submit" />
+</form> */}
